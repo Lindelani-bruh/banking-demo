a46a3b5446da25f69585c90099403c13a280ec71
lindelani <lindelani.ngwenya>
2024-03-17 22:35:25 +0200

    Add transfer unit test.

--- src/test/kotlin/demo/BankingDemoTests.kt
+++ src/test/kotlin/demo/BankingDemoTests.kt
@@ -2,6 +2,7 @@ package demo
 
 import com.trendyol.kediatr.Mediator
 import demo.application.deposit.commands.DepositCommand
+import demo.application.transfer.commands.TransferCommand
 import demo.domain.*
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
@@ -23,7 +24,6 @@ class BankingDemoTests (
 
     @Test
     fun `Assert deposit was successful, status code`() {
-
         val johnDoe = userRepository.save(Customer( "JohnB", "John", "Doe", "example-1@demo.com", "12345"))
         val account = accountRepository.save( Account(balance = 20000.00, type = "savings", owner = johnDoe.id.toString()) )
         val TEST_COMMAND = DepositCommand(2000.00, "ZAR", account.id.toString())
@@ -39,15 +39,22 @@ class BankingDemoTests (
     @Test
     fun `Assert transfer was successful, status code`() {
 
-        /*val johnDoe = userRepository.save(Customer( "JohnB", "John", "Doe", "12345"))
-        var account = accountRepository.save( Account(balance = 20000.00, type = "savings", owner = johnDoe.id.toString()) )
-        val TEST_COMMAND = DepositCommand(2000.00, "ZAR", account.id.toString())
+        val johnDoe = userRepository.save(Customer( "JohnB", "John", "Doe", "example-1@demo.com", "12345"))
+        val fromAccount = accountRepository.save( Account(balance = 20000.00, type = "savings", owner = johnDoe.id.toString()))
+
+        val blink180 = userRepository.save(Customer( "blink180", "blink", "180", "example-2@demo.com", "12345"))
+        val destinationAccount = accountRepository.save( Account(balance = 20000.00, type = "savings", owner = johnDoe.id.toString()))
+
+        val TEST_COMMAND = TransferCommand(2000.00, destinationAccount.id.toString(), "ZAR", fromAccount.id.toString())
 
         runBlocking {
             mediator.send(TEST_COMMAND)
         }
 
-        val results = accountRepository.findById(account.id.toString());
-        assertThat(results.get().balance).isEqualTo(22000.0)*/
+        val destinationAccountRESULT = accountRepository.findById(destinationAccount.id.toString());
+        val fromAccountRESULT = accountRepository.findById(fromAccount.id.toString());
+        assertThat(destinationAccountRESULT.get().balance).isEqualTo(22000.0)
+        assertThat(fromAccountRESULT.get().balance).isEqualTo(18000.0)
+
     }
 }
